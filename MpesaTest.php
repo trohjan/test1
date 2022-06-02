cbfe47f96863a5a3d14796a5cd17e385cb126911
trohjan <jamesobaigwa345@gmail.com>
2022-06-02 08:20:48 -0700

    commit 7

--- MpesaTest.php
+++ MpesaTest.php
@@ -10,7 +10,7 @@ $Token_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=clien
 $phone_number = $_POST['phone_number'];
 $OnlinePayment = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 $total_amount = $_POST['amount'];
-$CallBackURL = 'https://2f50f430.ngrok.io/callback.php?key=your password';
+$CallBackURL = 'https://damp-beach-80766.herokuapp.com/confirm.php?key=your password';
 $Time_Stamp = date("Ymdhis");
 $password = base64_encode($Business_Code . $Passkey . $Time_Stamp);
 ?>
