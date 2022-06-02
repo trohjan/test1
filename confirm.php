07750da649bd4d59fb3b29ca78871ec8d98affdd
trohjan <jamesobaigwa345@gmail.com>
2022-06-02 08:10:50 -0700

    commit 4

--- confirm.php
+++ confirm.php
@@ -1,3 +1,4 @@
+
 <form class="contact2-form validate-form" action="#" method="post">
    <input type="hidden" name="Check_request_ID" value="<?php echo $curl_Tranfer2_response->Check_request_ID ?>">
    </br></br>
